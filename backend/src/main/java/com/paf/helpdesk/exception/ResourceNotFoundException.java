9abc5c11ac8faf900e15d94e81722f2e97feddcb
Umesha Divyanganee <umeshadivyangani@gmail.com>
2026-04-22 18:15:54 +0530

    second commit

--- backend/src/main/java/com/paf/helpdesk/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/paf/helpdesk/exception/ResourceNotFoundException.java
@@ -5,4 +5,8 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
+    
+    public ResourceNotFoundException(Long id) {
+        super("Resource not found with id: " + id);
+    }
 }
