7c9441412b2b87841dced9e2f2eeb29e9b132711
Umesha Divyanganee <umeshadivyangani@gmail.com>
2026-04-23 09:34:25 +0530

    delete home page only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
 import ResourceListPage from "./pages/ResourceListPage";
 import ResourceFormPage from "./pages/ResourceFormPage";
 import ReportIssuePage from "./pages/ReportIssuePage";
@@ -17,7 +16,7 @@ function App() {
       <Header />
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<ResourceListPage />} />
           <Route path="/resources" element={<ResourceListPage />} />
           <Route path="/resources/add" element={<ResourceFormPage />} />
           <Route path="/resources/edit/:id" element={<ResourceFormPage />} />
